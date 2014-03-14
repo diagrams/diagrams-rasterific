c44af3eb28af9aab3bd8aaf87e9badb9f5cdcfd4
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-03-14 11:14:56 -0400

    CmdLine template

---
+++ src/Diagrams/Backend/Rasterific/CmdLine.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE CPP                  #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Diagrams.Backend.Rasterific.CmdLine
+-- Copyright   :  (c) 2014 Diagrams team (see LICENSE)
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  diagrams-discuss@googlegroups.com
+--
+-- Convenient creation of command-line-driven executables for
+-- rendering diagrams using the Rasterific backend.
