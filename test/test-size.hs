5c2e2019336ec6fd9146c4b9db3032389cfd14e0
Robert Vollmert <rob@vllmrt.net>
2018-07-20 12:22:07 +0200

    simpler test case
    a tall ellipse, which gets cut off in PDF with width specified only

--- test/test-size.hs
+++ test/test-size.hs
@@ -4,13 +4,13 @@ import           Diagrams.Backend.Rasterific
 {- non-square diagram should yield non-square output -}
 
 dia :: Diagram B
-dia = roundedRect 2 1 0.2 === strutY 0.2
+dia = ellipseXY 1 1.5
 
 sizeSpec :: SizeSpec V2 Double
-sizeSpec = mkSizeSpec2D (Just 600) Nothing
+sizeSpec = mkSizeSpec2D (Just 200) Nothing
 
 sizeSpecBoth :: SizeSpec V2 Double
-sizeSpecBoth = mkSizeSpec2D (Just 600) (Just 500)
+sizeSpecBoth = mkSizeSpec2D (Just 200) (Just 300)
 
 main :: IO ()
 main = do
